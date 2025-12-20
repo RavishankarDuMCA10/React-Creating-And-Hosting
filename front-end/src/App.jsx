bf218f861e764fd77350121fc14d15f7b542e332
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-20 22:21:17 +0530

    Used React Router links.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,13 +4,18 @@ import HomePage from './pages/HomePage'
 import ArticlesListPage from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
 import AboutPage from './pages/AboutPage';
+import Layout from './Layout';
 
-const routes = [
-  { path: '/',element: <HomePage /> },
-  { path: '/articles',element: <ArticlesListPage /> },
-  { path: '/article',element: <ArticlePage /> },
-  { path: '/about',element: <AboutPage /> }
-]
+const routes = [{
+  path: '/',
+  element: <Layout />,
+  children: [
+    { path: '/',element: <HomePage /> },
+    { path: '/articles',element: <ArticlesListPage /> },
+    { path: '/article',element: <ArticlePage /> },
+    { path: '/about',element: <AboutPage /> }
+  ]
+}]
 
 const router = createBrowserRouter(routes)
 
