468beab54c1e32c4fe256e4df6b9779442d5a56f
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-21 23:52:59 +0530

    Added upvoting articles.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,20 +1,33 @@
 import express from 'express';
 
+const articleInfo = [
+    { name: 'learn-react', upvotes: 0 },
+    { name: 'learn-node', upvotes: 0 },
+    { name: 'mangodb', upvotes: 0 }
+]
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', function (req, res) {
-    res.send('Hello form a GET endpoint!');
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const article = articleInfo.find(a => a.name === req.params.name)
+    article.upvotes += 1;
+    // res.status(200).send(`${article.upvotes}`);
+    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes.');
 });
 
-app.get('/hello/:name', function (req, res) {
-    res.send('Hello ' + req.params.name);
-});
+// app.get('/hello', function (req, res) {
+//     res.send('Hello form a GET endpoint!');
+// });
 
-app.post('/hello', function (req, res) {
-    res.send('Hello ' + req.body.name+' form a POST endpoint!');
-});
+// app.get('/hello/:name', function (req, res) {
+//     res.send('Hello ' + req.params.name);
+// });
+
+// app.post('/hello', function (req, res) {
+//     res.send('Hello ' + req.body.name+' form a POST endpoint!');
+// });
 
 app.listen(8000, function() {
     console.log('Server is listening on port 8000');
