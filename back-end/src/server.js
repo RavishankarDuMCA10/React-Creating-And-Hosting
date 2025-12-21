d688afe44a1f6d5198440353a181e60f782ab3c0
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-21 22:36:21 +0530

    Added changes for a POST method.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', function (req, res) {
-    res.send('Hello!');
+    res.send('Hello form a GET endpoint!');
+});
+
+app.post('/hello', function (req, res) {
+    res.send('Hello ' + req.body.name+' form a POST endpoint!');
 });
 
 app.listen(8000, function() {
