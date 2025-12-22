bb14cc3db0859950a16e9007ee1044897f364593
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-22 22:40:43 +0530

    Used the useLoaderData hook.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,21 +1,41 @@
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import './App.css'
-import HomePage from './pages/HomePage'
+import axios from 'axios';
+import './App.css';
+import HomePage from './pages/HomePage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 import Layout from './Layout';
 
+
 const routes = [{
   path: '/',
   element: <Layout />,
   errorElement: <NotFoundPage />,
   children: [
-    { path: '/',element: <HomePage /> },
-    { path: '/articles',element: <ArticlesListPage /> },
-    { path: '/articles/:name',element: <ArticlePage /> },
-    { path: '/about',element: <AboutPage /> }
+    { 
+      path: '/',
+      element: <HomePage /> 
+    },
+    { 
+      path: '/about',
+      element: <AboutPage />,
+    },
+    { 
+      path: '/articles',
+      element: <ArticlesListPage /> 
+    },
+    { 
+      path: '/articles/:name',
+      element: <ArticlePage />,
+
+      loader: async function() {
+        const response = await axios.get('/api/articles/learn-node');
+        const { upvotes, comments } = response.data;
+        return { upvotes, comments };
+      }
+    },    
   ]
 }]
 
