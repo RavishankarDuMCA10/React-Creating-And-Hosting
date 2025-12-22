86eadaa51e2a7bbcd37b4a48d8cf0d243446605c
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-22 17:19:55 +0530

    Added comment field for article.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const articleInfo = [
-    { name: 'learn-react', upvotes: 0 },
-    { name: 'learn-node', upvotes: 0 },
-    { name: 'mangodb', upvotes: 0 }
+    { name: 'learn-react', upvotes: 0, comments: [] },
+    { name: 'learn-node', upvotes: 0, comments: [] },
+    { name: 'mangodb', upvotes: 0, comments: [] }
 ]
 
 const app = express();
@@ -14,7 +14,21 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const article = articleInfo.find(a => a.name === req.params.name)
     article.upvotes += 1;
     // res.status(200).send(`${article.upvotes}`);
-    res.send('Hooray! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes.');
+    // res.send('Hooray! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes.');
+    res.json(article);
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const name = req.params.name;
+    const { postedBy, text } = req.body;
+
+    const article = articleInfo.find(a => a.name === name);
+    article.comments.push({
+        postedBy,
+        text,
+    });
+
+    res.json(article);
 });
 
 // app.get('/hello', function (req, res) {
