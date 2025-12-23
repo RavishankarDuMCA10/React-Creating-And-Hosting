bc8ee442e6e93c54b63598fa95d922362ef6dd32
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-23 23:29:05 +0530

    Added firebase auth settings.

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsj7nV7iidjBLwuY6ERtTyiPDkgA84sZU",
+  authDomain: "full-stack-react-f33f6.firebaseapp.com",
+  projectId: "full-stack-react-f33f6",
+  storageBucket: "full-stack-react-f33f6.firebasestorage.app",
+  messagingSenderId: "44411950310",
+  appId: "1:44411950310:web:1dd3a68aa8313dbdb51727",
+  measurementId: "G-60EE64KHN4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
---
+++ front-end/src/pages/CreateAccountPage.jsx
@@ -0,0 +1,7 @@
+export default function CreateAccountPage() {
+    return (
+        <main>
+            <h1>This is the Home Page!</h1>
+        </main>
+    );
+}
